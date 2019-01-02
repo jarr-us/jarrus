affca48486e4df20b39601e188963e1a6a2c10dd
Jason Creighton <jasoncrate@yahoo.com>
2019-01-02 08:34:51 -0500

    Added AlternatingPositionCrossover

---
+++ GeneticAlgorithmTests/Crossovers/Ordered/AlternatePositionCrossoverTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using GeneticAlgorithms.BasicTypes;
+using GeneticAlgorithms.Crossovers.Ordered;
+using GeneticAlgorithmTests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeneticAlgorithmTests.Crossovers.Ordered
+{
+    [TestClass]
+    public class AlternatePositionCrossoverTests
+    {
+        [TestMethod]
+        public void ItCanPerformCrossover()
+        {
+            var one = GATestHelper.GetNumericChromosomeOne();
+            var two = GATestHelper.GetNumericChromosomeTwo();
+
+            var cc = new AlternatingPositionCrossover();
+            var child = cc.Execute(one, two, GATestHelper.GetDefaultConfiguration<ExampleGene>());
+            
+            Assert.AreEqual("1,5,2,4,3,6,7", string.Join(",", (IEnumerable<Gene>)child.Genes));
+        }
+    }
+}
---
+++ GeneticAlgorithms/Crossovers/Ordered/AlternatingPositionCrossover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace GeneticAlgorithms.Crossovers.Ordered
+{
+    public class AlternatingPositionCrossover : Crossover
+    {
+        protected override Chromosome<T> Perform<T>(Chromosome<T> father, Chromosome<T> mother, GAConfiguration<T> settings)
+        {
+            var geneCount = father.Genes.Length;
+            var child = new Chromosome<T>(geneCount);
+
+            var seen = new List<T>();
+
+            for (int i = 0; i < geneCount; i++)
+            {
+                if (IsOdd(i))
+                {
+                    for (int k = 0; k < geneCount; k++)
+                    {
+                        if (!seen.Contains(mother.Genes[k]))
+                        {
+                            child.Genes[i] = mother.Genes[k];
+                            seen.Add(child.Genes[i]);
+                            break;
+                        }
+                    }
+                } else
+                {
+                    for (int k = 0; k < geneCount; k++)
+                    {
+                        if (!seen.Contains(father.Genes[k]))
+                        {
+                            child.Genes[i] = father.Genes[k];
+                            seen.Add(child.Genes[i]);
+                            break;
+                        }
+                    }                    
+                }
+            }
+
+            return child;
+        }
+
+        private static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+    }
+}
