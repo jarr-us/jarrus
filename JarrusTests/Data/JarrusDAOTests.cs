0a2edb129b2a87aea88901f839a83daab4d667e2
Jason Creighton <jasoncrate@yahoo.com>
2019-01-07 09:47:01 -0500

    small updates to tests

--- JarrusTests/Data/JarrusDAOTests.cs
+++ JarrusTests/Data/JarrusDAOTests.cs
@@ -49,74 +49,73 @@ namespace JarrusTests.Data
             Assert.AreEqual(LastName.Espinosa, currentBest.LastName);
             Assert.AreEqual(LastName.Espinosa, runDetails.BestChromosome.LastName);
         }
+        
+        [TestMethod]
+        public void ItCanInsertFullRandoms()
+        {
+            var tasks = new List<GATask>();
 
+            var random = new Random();
+            for (int i = 0; i < 5000; i++)
+            {
+                var task = new GATask(new OfficeSurveySolution())
+                {
+                    ParentSelectionType = ParentSelectionType.StochasticUniversalSamplingSelection,
+                    MutationType = MutationType.Swap,
+                    CrossoverType = CrossoverType.Cycle,
 
-        //[TestMethod]
-        //public void ItCanInsertFullRandoms()
-        //{
-        //    var tasks = new List<GATask>();
+                    MaxPopulationSize = 112,
+                    CrossoverRate = 0.96,
+                    MutationRate = 0.14,
+                    ElitismRate = 0.25,
+                    MaximumLifeSpan = 637,
+                    ChildrenPerCouple = 5,
 
-        //    var random = new Random();
-        //    for (int i = 0; i < 15000; i++)
-        //    {
-        //        var task = new GATask(new OfficeSurveySolution())
-        //        {
-        //            ParentSelectionType = ParentSelectionType.StochasticUniversalSamplingSelection,
-        //            MutationType = MutationType.Inversion,
-        //            CrossoverType = CrossoverType.Order,
+                    MaxGenerations = 20000,
+                    LowestScoreIsBest = false,
+                    PreventDuplications = true,
+                    RandomPoolGenerationSeed = 22,
 
-        //            MaxPopulationSize = 50,
-        //            CrossoverRate = 0.96,
-        //            MutationRate = 0.14,
-        //            ElitismRate = 0.25,
-        //            MaximumLifeSpan = 637,
-        //            ChildrenPerCouple = 5,
+                    Session = "0005"
+                };
 
-        //            MaxGenerations = 10000,
-        //            LowestScoreIsBest = false,
-        //            PreventDuplications = true,                   
-        //            RandomPoolGenerationSeed = 22,
+                var randomNumber = random.Next(1, 2 + 1);
+                if (randomNumber == 2) task.ParentSelectionType = ParentSelectionType.RouletteWheel;
 
-        //            Session = "0005"
-        //        };
+                randomNumber = random.Next(1, 4 + 1);
+                if (randomNumber == 2) { task.MutationType = MutationType.Insert; }
+                if (randomNumber == 3) { task.MutationType = MutationType.Scramble; }
+                if (randomNumber == 4) { task.MutationType = MutationType.Swap; }
 
-        //        var randomNumber = random.Next(1, 2 + 1);
-        //        if (randomNumber == 2) task.ParentSelectionType = ParentSelectionType.RouletteWheel;
+                randomNumber = random.Next(1, 4 + 1);
+                if (randomNumber == 2) { task.CrossoverType = CrossoverType.AlternatingPosition; }
+                if (randomNumber == 3) { task.CrossoverType = CrossoverType.Cycle; }
+                if (randomNumber == 4) { task.CrossoverType = CrossoverType.PartialMapped; }
 
-        //        randomNumber = random.Next(1, 4 + 1);
-        //        if (randomNumber == 2) { task.MutationType = MutationType.Insert; }
-        //        if (randomNumber == 3) { task.MutationType = MutationType.Scramble; }
-        //        if (randomNumber == 4) { task.MutationType = MutationType.Swap; }
+                randomNumber = random.Next(50, 150 + 1);
+                task.MaxPopulationSize = randomNumber;
 
-        //        randomNumber = random.Next(1, 4 + 1);
-        //        if (randomNumber == 2) { task.CrossoverType = CrossoverType.AlternatingPosition; }
-        //        if (randomNumber == 3) { task.CrossoverType = CrossoverType.Cycle; }
-        //        if (randomNumber == 4) { task.CrossoverType = CrossoverType.PartialMapped; }
+                randomNumber = random.Next(60000, 99000 + 1);
+                task.CrossoverRate = randomNumber * 0.00001;
 
-        //        randomNumber = random.Next(50, 150 + 1);
-        //        task.MaxPopulationSize = randomNumber;
+                randomNumber = random.Next(1000, 15000 + 1);
+                task.MutationRate = randomNumber * 0.00001;
 
-        //        randomNumber = random.Next(60000, 99000 + 1);
-        //        task.CrossoverRate = randomNumber * 0.00001;
+                randomNumber = random.Next(7000, 25000 + 1);
+                task.ElitismRate = randomNumber * 0.00001;
 
-        //        randomNumber = random.Next(1000, 25000 + 1);
-        //        task.MutationRate = randomNumber * 0.00001;
+                randomNumber = random.Next(500, 1300 + 1);
+                task.MaximumLifeSpan = randomNumber;
 
-        //        randomNumber = random.Next(1000, 25000 + 1);
-        //        task.ElitismRate = randomNumber * 0.00001;
+                randomNumber = random.Next(1, 8 + 1);
+                task.ChildrenPerCouple = randomNumber;
 
-        //        randomNumber = random.Next(500, 1500 + 1);
-        //        task.MaximumLifeSpan = randomNumber;
+                task.RandomSeed = random.Next();
+                tasks.Add(task);
+            }
 
-        //        randomNumber = random.Next(1, 8 + 1);
-        //        task.ChildrenPerCouple = randomNumber;
-
-        //        task.RandomSeed = random.Next();
-        //        tasks.Add(task);
-        //    }
-
-        //   // InsertTasksToDatabase(tasks, 5);
-        //}
+            InsertTasksToDatabase(tasks, 5);
+        }
 
         //[TestMethod]
         //public void ItCanInsertSessionSixes()
